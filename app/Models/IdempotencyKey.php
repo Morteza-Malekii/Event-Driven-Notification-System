c51fd3556b3eea8a2b0cf0ab0712cb1c273e56d4
morteza <mortezaghube@gmail.com>
2026-03-06 19:15:25 +0300

    feat(models): add IdempotencyKey model

---
+++ app/Models/IdempotencyKey.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class IdempotencyKey extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'key',
+        'request_hash',
+        'notification_id',
+        'response_cache',
+        'expires_at',
+    ];
+
+    protected $casts = [
+        'response_cache' => 'array',
+        'expires_at'     => 'datetime',
+    ];
+}

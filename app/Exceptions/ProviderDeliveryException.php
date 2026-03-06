15745b322081f154a29c2cc2e7957509b4764e44
morteza <mortezaghube@gmail.com>
2026-03-06 19:41:53 +0300

    feat(exceptions): add ProviderDeliveryException with isPermanent flag

---
+++ app/Exceptions/ProviderDeliveryException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use RuntimeException;
+
+class ProviderDeliveryException extends RuntimeException
+{
+    public function __construct(
+        string                   $message,
+        public readonly bool     $isPermanent,
+        ?\Throwable              $previous = null,
+    ) {
+        parent::__construct($message, 0, $previous);
+    }
+}
